d80e583bcb00ea737b85b8879b2e929e132b6fc6
Jenyaya <jenyaya@online.ua>
2012-02-15 13:49:47 +0000

    precompile=false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,8 @@ Pages::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
-  config.assets.precompile += %w( blueprint/screen.css blueprint/print.css blueprint/ie.css )
+ # config.assets.precompile += %w( blueprint/screen.css blueprint/print.css blueprint/ie.css )
+ config.assets.precompile += ['custom.css']
 
   # Generate digests for assets URLs
   config.assets.digest = true
